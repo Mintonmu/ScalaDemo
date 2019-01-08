29fb600c0c3677c0301733c04a8d5206808490b6
牟明 <2859595067@qq.com>
2019-01-08 17:53:02 +0800

    fghh

---
+++ src/main/scala/com/muming/chapter01/vars/VarDemo01.scala
@@ -0,0 +1,7 @@
+package com.muming.chapter01.vars
+
+object VarDemo01 {
+  def main(args: Array[String]): Unit = {
+    println("hello world")
+  }
+}
---
+++ src/main/scala/com/muming/chapter01/vars/VarDemo02.scala
@@ -0,0 +1,12 @@
+package com.muming.chapter01.vars
+
+object VarDemo02 {
+  def main(args: Array[String]): Unit = {
+    var age = 10
+    age = 15
+    val num = 30
+    //num = 10
+    // 使用val线程安全因此效率比较高，
+  }
+
+}
