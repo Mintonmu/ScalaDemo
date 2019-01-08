b2565be19c8148c936da3dc47f79a29dd668b654
牟明 <2859595067@qq.com>
2019-01-08 18:16:02 +0800

    learning scala one day

--- src/main/scala/com/muming/chapter01/vars/VarDemo02.scala
+++ src/main/scala/com/muming/chapter01/vars/VarDemo02.scala
@@ -6,7 +6,19 @@ object VarDemo02 {
     age = 15
     val num = 30
     //num = 10
-    // 使用val线程安全因此效率比较高，
+    // 使用val线程安全因此效率比较高，但是val修饰的变量是不能被改变的，相当于加final修饰
+    // 如果需要改变当前定义的对象则应该使用var
+
+    val dog = new Dog
+    dog.age = 10
+    dog.name = "小黑"
   }
 
+}
+
+class Dog {
+  // 声明一个变量需要对该变量初始化
+  // 声明一个age属性，一个默认值_
+  var age: Int = _
+  var name: String = ""
 }
---
+++ src/main/scala/com/muming/chapter02/datatype/TypeDemo01.scala
@@ -0,0 +1,12 @@
+package com.muming.chapter02.datatype
+
+object TypeDemo01 {
+
+  def main(args: Array[String]): Unit = {
+    var num1: Int = 10
+    // 因为Int是一个类，因此他的实例拥有很多方法
+    // scala中方法没有形参则可以省略（）运算符
+
+  }
+
+}
