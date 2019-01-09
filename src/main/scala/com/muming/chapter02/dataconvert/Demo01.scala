5a069200ff3a300c502f56498f43eacd4fc8aff7
牟明 <2859595067@qq.com>
2019-01-09 15:35:54 +0800

    learning scala two day

---
+++ src/main/scala/com/muming/chapter02/dataconvert/Demo01.scala
@@ -0,0 +1,13 @@
+package com.muming.chapter02.dataconvert
+
+object Demo01 {
+  def main(args: Array[String]): Unit = {
+
+    var n1 = 10
+
+    var n2 = 1.1f
+    var n3 = n1 + n2
+
+
+  }
+}
---
+++ src/main/scala/com/muming/chapter03/Input/InputDemo.scala
@@ -0,0 +1,10 @@
+package com.muming.chapter03.Input
+
+object InputDemo {
+  def main(args: Array[String]): Unit = {
+    for (i <- 0 until 10) {
+      println(i)
+    }
+  }
+
+}
