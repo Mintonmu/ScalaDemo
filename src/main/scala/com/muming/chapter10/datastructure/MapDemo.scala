c8aef7185203e759dacccfec6b480eddcb7c14b2
牟明 <2859595067@qq.com>
2019-01-27 13:48:32 +0800

    data structure

--- src/main/scala/com/muming/chapter10/datastructure/MapDemo.scala
+++ src/main/scala/com/muming/chapter10/datastructure/MapDemo.scala
@@ -14,8 +14,24 @@ object MapDemo {
 		val map2 = mutable.Map("Alice" -> 20, "Bob" -> 30)
 		map2.foreach(println)
 
+		//对偶元组
 		val map3 = mutable.Map[String, Int](("A", 1), ("B", 2))
 
+
+		if (map1.contains("Aile")) {
+			println(map1("Aile"))
+		}
+		else {
+			throw new Exception
+		}
+		println(map2)
+		println(map2.getOrElse("Ailce", 30))
+
+		for ((k, v) <- map1) {
+
+			println(k + "  " + v)
+		}
 	}
+
 
 }
---
+++ src/main/scala/com/muming/chapter10/datastructure/SetDemo.scala
@@ -0,0 +1,13 @@
+package com.muming.chapter10.datastructure
+
+import scala.collection.mutable
+
+object SetDemo extends App {
+
+
+	val set1 = Set(1, 2, 3)
+
+	val set2 = mutable.Set(4, 5, 6, 7)
+
+
+}
